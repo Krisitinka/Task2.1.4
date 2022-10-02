21bf705ea692b743b923d92367ea6ccfc99f0d60
belya <belyakova.kristinochcka@yandex.ru>
2022-10-03 00:20:04 +0500

    Death

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+        private Egg6 egg;
+        public Duck5(Egg6 egg){
+            this.egg=egg;
+        }
 
     @Override
     public String toString() {
